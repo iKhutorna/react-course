9ed472bfaf0b1793ff9378875a343cb0567a96a0
iKhutorna <iruna_kh@ukr.net>
2020-08-06 14:50:26 +0300

    components added

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const el = (
-  <div>
-    <h1>My Todo List</h1>
-    <input type="search" placeholder="search"/>
+const TodoList = () => {
+  return (
     <ul>
       <li>Create Todo list</li>
       <li>Learn React</li>
     </ul>
-  </div>
-);
+  );
+};
 
-ReactDom.render(el, document.getElementById('root'));
+const AppHeader = () => {
+  return (
+    <h1>My Todo List</h1>
+  );
+};
+
+const SearchPannel = () => {
+  return (
+    <input type="search" placeholder="search"/>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader/>
+      <SearchPannel/>
+      <TodoList/>
+    </div>
+  );
+};
+
+ReactDom.render(<App/>, document.getElementById('root'));
