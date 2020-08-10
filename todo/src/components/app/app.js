0c60e49e0ecd07b8808b17d1e0a83138de286751
iKhutorna <iruna_kh@ukr.net>
2020-08-10 23:47:48 +0300

    todo: new structure added

---
+++ todo/src/components/app/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import AppHeader from "../app-header";
+import SearchPannel from "../search-panel";
+import TodoList from "../todo-list";
+import ItemStatusFilter from '../item-status-filter';
+
+import './app.css';
+
+const todoData = [
+  { label: 'drink coffee', important: false, id: 1 },
+  { label: 'Make Awesome App', important: true, id: 2 },
+  { label: 'Have a lunch', important: false, id:3 }
+];
+
+const App = () => {
+  return (
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3}/>
+      <div className="top-panel d-flex">
+        <SearchPannel/>
+        <ItemStatusFilter />
+      </div>
+      <TodoList todos={ todoData }/>
+    </div>
+  );
+};
+
+export default App;
