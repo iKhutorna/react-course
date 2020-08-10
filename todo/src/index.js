fd51fd7c6afdd586e4b07b46feecc75a012635bf
iKhutorna <iruna_kh@ukr.net>
2020-08-10 10:26:59 +0300

    arrays as props

--- todo/src/index.js
+++ todo/src/index.js
@@ -5,12 +5,18 @@ import AppHeader from "./components/app-header";
 import SearchPannel from "./components/search-panel";
 import TodoList from "./components/todo-list";
 
+const todoData = [
+  { label: 'drink coffee', important: false },
+  { label: 'Make Awesome App', important: true },
+  { label: 'Have a lunch', important: false }
+];
+
 const App = () => {
   return (
     <div>
       <AppHeader/>
       <SearchPannel/>
-      <TodoList/>
+      <TodoList todos={ todoData }/>
     </div>
   );
 };
