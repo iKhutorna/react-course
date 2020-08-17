6f46ad91e2f8f6a70fe2d2157fdd10d5566ce8b9
iKhutorna <iruna_kh@ukr.net>
2020-08-17 18:16:01 +0300

    todo: setState added

--- todo/src/components/TodoListItem/todo-list-item.js
+++ todo/src/components/TodoListItem/todo-list-item.js
@@ -3,44 +3,64 @@ import "./todo-list-item.sass";
 
 export default class TodoListItem extends Component {
 
+  state = {
+    done: false,
+    important: false
+  };
   onLabelClick = () => {
-    console.log(`Done: ${this.props.label}`);
+    this.setState({
+      done: true
+    });
+  };
+  onMarkImportant = () => {
+    this.setState({
+      important: true
+    });
   };
 
   // constructor() {
   //   super();
+  //   this.state = {
+  //     done: false
+  //   };
   //   this.onLabelClick = () => {
   //     console.log(`Done: ${this.props.label}`)
   //   }
   // }
-  
-  render () {
-    const {label, important=false} = this.props;
 
-    const style = {
-      color: important ? "steelblue" : "black",
-      fontWeight: important ? "bold" : "normal"
-    };
+  render () {
+    const {label} = this.props;
+    const {done, important} = this.state;
+
+    let classNames = "todo-list-item";
+
+    if (done) {
+      classNames += " done";
+    }
+
+    if (important) {
+        classNames += " important";
+    }
 
     return (
-      <span>
-      <span
-        className="todo-list-item"
-        style={style}
-        onClick={this.onLabelClick}>
-        {label}
+      <span className={classNames}>
+        <span
+          className="todo-list-item-label"
+          onClick={this.onLabelClick}>
+          {label}
+        </span>
+
+        <button type="button"
+          className="btn btn-outline-success btn-sm float-right"
+          onClick={this.onMarkImportant}>
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button type="button"
+          className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
       </span>
-
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
     );
   }
 };
