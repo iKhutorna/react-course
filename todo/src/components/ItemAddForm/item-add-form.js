c375eff96b3c7628205a69a9a38c939b9efd6b33
iKhutorna <iruna_kh@ukr.net>
2020-08-27 13:21:29 +0300

    todo: item-add-form made controlled

--- todo/src/components/ItemAddForm/item-add-form.js
+++ todo/src/components/ItemAddForm/item-add-form.js
@@ -8,13 +8,16 @@ export default class ItemAddForm extends Component {
 
   onLabelChange = (e) => {
     this.setState({
-      label: e.target.value
+      label: e.target.value.toUpperCase()
     });
   };
 
   onSubmit = (e) => {
     e.preventDefault();
     this.props.addItem(this.state.label);
+    this.setState({
+      label: ""
+    });
   };
 
   render() {
@@ -25,7 +28,8 @@ export default class ItemAddForm extends Component {
         <input type="text"
           className="form-control"
           onChange={this.onLabelChange}
-          placeholder="What needs to be done"/>
+          placeholder="What needs to be done"
+          value = {this.state.label}/>
         <button type="button" className="btn btn-outline-secondary"
         onClick={()=>addItem("Hello world")}>
           Add item
