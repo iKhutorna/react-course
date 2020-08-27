873838087929c2e22c5315d17826a33ab7209bfc
iKhutorna <iruna_kh@ukr.net>
2020-08-27 12:59:59 +0300

    todo: form added

--- todo/src/components/ItemAddForm/item-add-form.js
+++ todo/src/components/ItemAddForm/item-add-form.js
@@ -2,15 +2,35 @@ import React, {Component} from "react";
 import "./item-add-form.sass";
 
 export default class ItemAddForm extends Component {
+  state = {
+    label: ""
+  };
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.addItem(this.state.label);
+  };
+
   render() {
     const {addItem} = this.props;
     return (
-      <div className="item-add-form">
+      <form className="item-add-form d-flex"
+        onSubmit = {this.onSubmit}>
+        <input type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="What needs to be done"/>
         <button type="button" className="btn btn-outline-secondary"
         onClick={()=>addItem("Hello world")}>
           Add item
         </button>
-      </div>
+      </form>
     );
   };
 };
