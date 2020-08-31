860bc88a867e78dcd7f5ef249ea79bf86d688585
iKhutorna <iruna_kh@ukr.net>
2020-08-31 10:59:07 +0300

    todo: fileter logic updated

--- todo/src/components/ItemStatusFilter/item-status-filter.js
+++ todo/src/components/ItemStatusFilter/item-status-filter.js
@@ -3,27 +3,86 @@ import "./item-status-filter.sass";
 
 export default class ItemStatusFilter extends Component {
 
+
+
+  onClickAll = () => {
+    this.props.onShowAll();
+    this.setState(({buttons}) => {
+      const newArr = buttons.map((el) => {
+        if(el.key === "all") {
+          el.active = true;
+        } else {
+          el.active = false;
+        }
+        return el;
+      });
+      return {
+        buttons: newArr
+      }
+    });
+  };
+
+  onClickActive = () => {
+    this.props.onShowActive();
+    this.setState(({buttons}) => {
+      const newArr = buttons.map((el) => {
+        if(el.key === "active") {
+          el.active = true;
+        } else {
+          el.active = false;
+        }
+        return el;
+      });
+      return {
+        buttons: newArr
+      }
+    });
+  };
+
+  onClickDone = () => {
+    this.props.onShowDone();
+    this.setState(({buttons}) => {
+      const newArr = buttons.map((el) => {
+        if(el.key === "done") {
+          el.active = true;
+        } else {
+          el.active = false;
+        }
+        return el;
+      });
+      return {
+        buttons: newArr
+      }
+    });
+  };
+
+  state = {
+    buttons: [
+      {key: "all", label: "All", active: true, method: this.onClickAll},
+      {key: "active", label: "Active", active: false, method: this.onClickActive},
+      {key: "done", label: "Done", active: false, method: this.onClickDone}
+    ]
+  };
+
   render () {
-    const {onShowActive, onShowDone, onShowAll} = this.props;
-    let classNames = "btn btn-outline-secondary";
+    const elements = this.state.buttons.map((item) => {
+      let classNames = "btn btn-outline-secondary";
+      if(item.active) {
+        classNames += " active";
+      }
+      return (
+        <button type="button"
+                key={item.key}
+                className={classNames}
+                onClick={item.method}>
+          {item.label}
+        </button>
+      );
+    });
 
     return (
       <div className="btn-group">
-        <button type="button"
-                className={classNames}
-                onClick={onShowAll}>
-          All
-        </button>
-        <button type="button"
-                className={classNames}
-                onClick={onShowActive}>
-          Active
-        </button>
-        <button type="button"
-                className={classNames}
-                onClick={onShowDone}>
-          Done
-        </button>
+        {elements}
       </div>
     );
   }
