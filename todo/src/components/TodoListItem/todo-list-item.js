b3eea01d80d6597c1890f9c9bd440e97cb37173f
iKhutorna <iruna_kh@ukr.net>
2020-08-31 14:48:38 +0300

    todo: styles fixed

--- todo/src/components/TodoListItem/todo-list-item.js
+++ todo/src/components/TodoListItem/todo-list-item.js
@@ -21,25 +21,27 @@ export default class TodoListItem extends Component {
     }
 
     return (
-      <span className={classNames}>
-        <span
+      <div className={classNames}>
+        <div
           className="todo-list-item-label"
           onClick={onToggleDone}>
           {label}
-        </span>
+        </div>
 
-        <button type="button"
-          className="btn btn-outline-success btn-sm float-right"
-          onClick={onToggleImportant}>
-          <i className="fa fa-exclamation" />
-        </button>
+        <div className="todo-list-item-buttons">
+          <button type="button"
+            className="btn btn-outline-success btn-sm float-right"
+            onClick={onToggleImportant}>
+            <i className="fa fa-exclamation" />
+          </button>
 
-        <button type="button"
-          className="btn btn-outline-danger btn-sm float-right"
-          onClick={onDeleted}>
-          <i className="fa fa-trash-o" />
-        </button>
-      </span>
+          <button type="button"
+            className="btn btn-outline-danger btn-sm float-right"
+            onClick={onDeleted}>
+            <i className="fa fa-trash-o" />
+          </button>
+        </div>
+      </div>
     );
   }
 };
